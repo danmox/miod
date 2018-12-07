5e53ec2b9d748fa2793c90250d85be6ef56bfdba
Daniel Mox <mox@seas.upenn.edu>
2018-12-07 12:02:54 -0500

    Latest changes post Oct 2018 Intel meeting

---
+++ network_status/src/intel_demo.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>
+
+#include <geometry_msgs/Pose.h>
+#include <intel_aero_navigation/WaypointNavigationAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+#include <vector>
+#include <string.h>
+#include <sstream>
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "demo_node");
+  ros::NodeHandle nh, gnh("/");
+
+  int number_of_robots, task_agent_id;
+  if (!gnh.getParam("number_of_agents", number_of_robots) ||
+      !gnh.getParam("task_agent_id", task_agent_id)) {
+    ROS_FATAL("[demo_node] unable to fetch params from server");
+    return -1;
+  }
+
+  typedef intel_aero_navigation::WaypointNavigationAction NavAction;
+  typedef actionlib::SimpleActionClient<NavAction> NavClient;
+  std::vector<std::shared_ptr<NavClient>> nav_clients;
+  for (int i = 1; i <= number_of_robots; ++i) {
+    std::stringstream ss;
+    ss << "/aero" << i << "/waypoint_navigation_nodelet";
+    std::string sn = ss.str();
+    std::shared_ptr<NavClient> ac_ptr(new NavClient(sn.c_str(), true));
+    ROS_INFO("[demo_node] waiting for %s action server to start", sn.c_str());
+    ac_ptr->waitForServer();
+    ROS_INFO("[demo_node] %s action server ready", sn.c_str());
+    nav_clients.push_back(ac_ptr);
+  }
+
+  double desired_altitude = 2.0;
+  std::vector<geometry_msgs::Pose> start_poses;
+  geometry_msgs::Pose goal;
+  goal.orientation.w = 1.0;
+  goal.position.z = desired_altitude;
+
+  // agent 1
+  goal.position.x = 30;
+  goal.position.y = 5;
+  start_poses.push_back(goal);
+
+  // agent 2
+  goal.position.x = 55;
+  goal.position.y = 18;
+  start_poses.push_back(goal);
+
+  // agent 3
+  goal.position.x = 75;
+  goal.position.y = 33;
+  start_poses.push_back(goal);
+
+  // agent 4
+  goal.position.x = 63;
+  goal.position.y = -10;
+  start_poses.push_back(goal);
+
+  // agent 5
+  goal.position.x = 35;
+  goal.position.y = -17;
+  start_poses.push_back(goal);
+
+  // agent 6
+  goal.position.x = 27;
+  goal.position.y = 0;
+  start_poses.push_back(goal);
+
+  ROS_INFO("[demo_node] starting quads");
+  for (int i = 0; i < number_of_robots; ++i) {
+    intel_aero_navigation::WaypointNavigationGoal goal_msg;
+    goal_msg.goal = start_poses[i];
+    nav_clients[i]->sendGoal(goal_msg);
+  }
+
+  ROS_INFO("[demo_node] waiting for goals to be complete");
+  for (int i = 0; i < number_of_robots; ++i) {
+    nav_clients[i]->waitForResult();
+  }
+  ROS_INFO("[demo_node] completed takeoff");
+
+  return 0;
+}
--- network_status/src/network_visualization_node.cpp
+++ network_status/src/network_visualization_node.cpp
@@ -1,14 +1,18 @@
-//#include <communication_predict/CommunicationPredict.h>
+#include <CommunicationPredict.h>
 
 #include <ros/ros.h>
+#include <std_msgs/Float64.h>
 #include <tf2_ros/transform_listener.h>
 #include <visualization_msgs/Marker.h>
 #include <std_msgs/ColorRGBA.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Vector3.h>
+#include <network_status/RatePair.h>
 #include <string.h>
 #include <sstream>
+#include <vector>
+
 
 double distance(const geometry_msgs::Point p1, const geometry_msgs::Point p2)
 {
@@ -16,12 +20,15 @@ double distance(const geometry_msgs::Point p1, const geometry_msgs::Point p2)
               + pow(p2.z - p1.z, 2.0));
 }
 
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "network_visualization_node");
   ros::NodeHandle nh, pnh("~");
 
   ros::Publisher viz_pub = nh.advertise<visualization_msgs::Marker>("network_visualization", 10);
+  ros::Publisher rates_pub = nh.advertise<network_status::RatePair>("channel_rates", 10);
+  ros::Publisher task_pub = nh.advertise<std_msgs::Float64>("task_agent_rate", 10);
 
   tf2_ros::Buffer tf2_buff;
   tf2_ros::TransformListener tf2_listener(tf2_buff);
@@ -29,36 +36,33 @@ int main(int argc, char** argv)
   int number_of_agents, task_agent_id;
   std::string world_frame;
   double dist_threshold(0.0);
-  if (!pnh.getParam("number_of_agents", number_of_agents) ||
-      !pnh.getParam("task_agent_id", task_agent_id) ||
-      !pnh.getParam("world_frame", world_frame) ||
-      !pnh.getParam("dist_threshold", dist_threshold)) {
+  if (!nh.getParam("/number_of_agents", number_of_agents) ||
+      !nh.getParam("/task_agent_id", task_agent_id) ||
+      !pnh.getParam("world_frame", world_frame)) {
     ROS_FATAL("[network_visualization_node] failed to fetch parameter(s) from server");
     return -1;
   }
 
-  std_msgs::ColorRGBA green;
-  green.g = 1.0;
-  green.a = 1.0;
-
-  std_msgs::ColorRGBA red;
-  red.r = 1.0;
-  red.a = 1.0;
+  bool use_map = true;
+  CommunicationPredict comm_sim(use_map);
 
   geometry_msgs::Vector3 scale;
   scale.x = 0.5;
 
-  /*
-  ROS_INFO("[network_visualization_node] giving other nodes some time to start");
-  ros::Rate countdown(1);
-  for (int i = 0; i < 10; ++i) {
-    countdown.sleep();
-    ros::spinOnce();
+  // compute pair permutations
+  std::vector<network_status::RatePair> perms;
+  for (int i = 1; i < number_of_agents; ++i) {
+    for (int j = i+1; j <= number_of_agents; ++j) {
+      network_status::RatePair pair;
+      pair.id1 = i;
+      pair.id2 = j;
+      perms.push_back(pair);
+    }
   }
-  */
 
   ros::Rate rate(10);
   ROS_INFO("[network_visualization_node] starting loop");
+  std_msgs::Float64 task_rate;
   while (ros::ok()) {
     rate.sleep();
     ros::spinOnce();
@@ -92,36 +96,56 @@ int main(int argc, char** argv)
       marker.header.stamp = ros::Time::now();
       marker.ns = "viz";
       marker.id = 1;
+      marker.lifetime = ros::Duration(1.0);
       marker.type = visualization_msgs::Marker::LINE_LIST;
-      marker.action = visualization_msgs::Marker::DELETEALL;
-      marker.color.a = 1.0;
-      viz_pub.publish(marker);
-
       marker.action = visualization_msgs::Marker::ADD;
       marker.scale = scale;
-      for (int i = 0; i < number_of_agents; ++i) {
-        for (int j = 0; j < number_of_agents; ++j) {
-          if (i == j || distance(node_location[i], node_location[j]) > dist_threshold)
-            continue;
+      for (auto& pair : perms) {
+        int i = pair.id1-1; // subtract to convert from names to indices
+        int j = pair.id2-1;
 
-          marker.points.push_back(node_location[i]);
-          marker.points.push_back(node_location[j]);
+        marker.points.push_back(node_location[i]);
+        marker.points.push_back(node_location[j]);
 
-          if (i == task_agent_id || j == task_agent_id) {
-            marker.colors.push_back(green);
-            marker.colors.push_back(green);
-          } else {
-            marker.colors.push_back(red);
-            marker.colors.push_back(red);
-          }
+        comm_sim.Predict(node_location[i].x, node_location[i].y,
+                         node_location[j].x, node_location[j].y,
+                         pair.rate, pair.std);
+
+        std_msgs::ColorRGBA color;
+        color.a = pair.rate;
+        ROS_DEBUG("[network_visualization_node] alpha = %.2f", pair.rate);
+
+        if (i == task_agent_id-1 || j == task_agent_id-1) {
+          if (pair.rate > task_rate.data)
+            task_rate.data = pair.rate;
+
+          color.g = 1.0;
+          marker.colors.push_back(color);
+          marker.colors.push_back(color);
+        } else {
+          color.r = 1.0;
+          marker.colors.push_back(color);
+          marker.colors.push_back(color);
         }
       }
+      task_pub.publish(task_rate);
+      task_rate.data = 0.0;
 
       viz_pub.publish(marker);
     } else {
       ROS_WARN("[network_visualization_node] not publishing this iteration");
     }
   }
+
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = world_frame;
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "viz";
+  marker.id = 1;
+  marker.type = visualization_msgs::Marker::LINE_LIST;
+  marker.action = visualization_msgs::Marker::DELETEALL;
+  marker.color.a = 1.0;
+  viz_pub.publish(marker);
 
   return 0;
 }
