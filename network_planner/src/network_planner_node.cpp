9fd3ee822158a3682b7ec50c7ea38a1c7fb497fe
Miguel Calvo-Fullana <cfullana@seas.upenn.edu>
2019-09-14 18:58:59 -0400

    added demo launch files

--- network_planner/src/network_planner_node.cpp
+++ network_planner/src/network_planner_node.cpp
@@ -35,19 +35,89 @@ int main(int argc, char** argv)
   network_planner::NetworkPlanner np(nh, pnh);
 
   // initialize task spec
+  // network_planner::CommReqs comm_reqs;
+  // network_planner::Flow flow1;
+  // flow1.srcs.insert(2);
+  // flow1.dests.insert(1);
+  // flow1.min_margin = margin1;
+  // flow1.confidence = confidence1;
+  // comm_reqs.push_back(flow1);
+  // network_planner::Flow flow2;
+  // flow2.srcs.insert(1);
+  // flow2.dests.insert(2);
+  // flow2.min_margin = margin2;
+  // flow2.confidence = confidence2;
+  // comm_reqs.push_back(flow2);
+
+  // Three flows with 2 destinations each
   network_planner::CommReqs comm_reqs;
   network_planner::Flow flow1;
-  flow1.srcs.insert(2);
-  flow1.dests.insert(1);
+  flow1.srcs.insert(1);
+  flow1.dests.insert(2);
+  flow1.dests.insert(3);  
   flow1.min_margin = margin1;
   flow1.confidence = confidence1;
   comm_reqs.push_back(flow1);
+
   network_planner::Flow flow2;
-  flow2.srcs.insert(1);
-  flow2.dests.insert(2);
+  flow2.srcs.insert(2);
+  flow2.dests.insert(1);
+  flow2.dests.insert(3);
   flow2.min_margin = margin2;
   flow2.confidence = confidence2;
   comm_reqs.push_back(flow2);
+
+  network_planner::Flow flow3;
+  flow3.srcs.insert(3);
+  flow3.dests.insert(1);
+  flow3.dests.insert(2);
+  flow3.min_margin = margin2;
+  flow3.confidence = confidence2;
+  comm_reqs.push_back(flow3);
+
+  // Everywhere flow
+  //network_planner::CommReqs comm_reqs;
+  // network_planner::Flow flow12;
+  // flow12.srcs.insert(1);
+  // flow12.dests.insert(2);
+  // flow12.min_margin = margin1;
+  // flow12.confidence = confidence1;
+  // comm_reqs.push_back(flow12);
+
+  // network_planner::Flow flow13;
+  // flow13.srcs.insert(1);
+  // flow13.dests.insert(3);  
+  // flow13.min_margin = margin1;
+  // flow13.confidence = confidence1;
+  // comm_reqs.push_back(flow13);
+
+  // network_planner::Flow flow21;
+  // flow21.srcs.insert(2);
+  // flow21.dests.insert(1);
+  // flow21.min_margin = margin2;
+  // flow21.confidence = confidence2;
+  // comm_reqs.push_back(flow21);
+
+  // network_planner::Flow flow23;
+  // flow23.srcs.insert(2);
+  // flow23.dests.insert(3);
+  // flow23.min_margin = margin2;
+  // flow23.confidence = confidence2;
+  // comm_reqs.push_back(flow23);
+
+  // network_planner::Flow flow31;
+  // flow31.srcs.insert(3);
+  // flow31.dests.insert(1);
+  // flow31.min_margin = margin2;
+  // flow31.confidence = confidence2;
+  // comm_reqs.push_back(flow31);
+
+  // network_planner::Flow flow32;
+  // flow32.srcs.insert(3);
+  // flow32.dests.insert(2);
+  // flow32.min_margin = margin2;
+  // flow32.confidence = confidence2;
+  // comm_reqs.push_back(flow32);
 
   np.setCommReqs(comm_reqs);
 
