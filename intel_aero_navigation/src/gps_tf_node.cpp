f0a7438a9309310c453329daada48097d3b7a5d1
Daniel Mox <mox@seas.upenn.edu>
2019-11-02 13:13:38 -0400

    GPS TF reports altitude relative to first message

--- intel_aero_navigation/src/gps_tf_node.cpp
+++ intel_aero_navigation/src/gps_tf_node.cpp
@@ -34,10 +34,9 @@ class GPSTF
     std::shared_ptr<message_filters::Subscriber<sensor_msgs::Imu>> imu_sub;
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::NavSatFix, sensor_msgs::Imu> Policy;
     typedef message_filters::Synchronizer<Policy> ApproxSync;
-    std::shared_ptr<ApproxSync> imu_gps_sync;
+    std::shared_ptr<ApproxSync> approx_sync;
 
-    std::string base_frame; // tree is: datum -> base_link
-    double yaw_offset;           // correction to make compass coincide with heading
+    std::string base_frame, world_frame; // tf tree will be: world_frame -> base_link
 
     tf2::Transform utm_datum_tf;
     std::string datum_utm_zone;
@@ -76,21 +75,20 @@ GPSTF::GPSTF(const ros::NodeHandle& nh_, ros::NodeHandle& pnh_) :
 
   gps_sub.reset(new message_filters::Subscriber<sensor_msgs::NavSatFix>(nh, "gps", 1));
   imu_sub.reset(new message_filters::Subscriber<sensor_msgs::Imu>(nh, "imu", 1));
-  imu_gps_sync.reset(new ApproxSync(Policy(10), *gps_sub, *imu_sub));
-  imu_gps_sync->registerCallback(std::bind(&GPSTF::syncCB,
-                                           this,
-                                           std::placeholders::_1,
-                                           std::placeholders::_2));
+  approx_sync.reset(new ApproxSync(Policy(10), *gps_sub, *imu_sub));
+  approx_sync->registerCallback(std::bind(&GPSTF::syncCB,
+                                          this,
+                                          std::placeholders::_1,
+                                          std::placeholders::_2));
 
   // vital ROS params
 
-  double datum_lat, datum_lon, datum_yaw, datum_alt;
+  double datum_lat, datum_lon, datum_yaw;
   getParamStrict(pnh, "base_frame", base_frame);
-  getParamStrict(pnh, "yaw_offset", yaw_offset);
+  getParamStrict(pnh, "world_frame", world_frame);
   getParamStrict(pnh, "datum_lat", datum_lat);
   getParamStrict(pnh, "datum_lon", datum_lon);
   getParamStrict(pnh, "datum_yaw", datum_yaw);
-  getParamStrict(pnh, "datum_alt", datum_alt);
 
   // compute datum transformation: utm -> datum
 
@@ -98,7 +96,7 @@ GPSTF::GPSTF(const ros::NodeHandle& nh_, ros::NodeHandle& pnh_) :
   LLtoUTM(datum_lat, datum_lon, utm_y, utm_x, datum_utm_zone);
   ROS_DEBUG("datum UTM zone: %s", datum_utm_zone.c_str());
 
-  utm_datum_tf.setOrigin(tf2::Vector3(utm_x, utm_y, datum_alt));
+  utm_datum_tf.setOrigin(tf2::Vector3(utm_x, utm_y, 0.0));
   utm_datum_tf.setRotation(tf2::Quaternion(tf2::Vector3(0,0,1), datum_yaw));
 }
 
@@ -107,6 +105,7 @@ void GPSTF::syncCB(const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
                    const sensor_msgs::Imu::ConstPtr& imu_msg)
 {
   static tf2_ros::TransformBroadcaster tf_broadcaster;
+  static sensor_msgs::NavSatFix initial_gps_fix = *gps_msg;
 
   double utm_x, utm_y;
   std::string gps_utm_zone;
@@ -116,7 +115,8 @@ void GPSTF::syncCB(const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
 
   // translation of utm -> base_link transformation
   tf2::Transform utm_base_link_tf;
-  utm_base_link_tf.setOrigin(tf2::Vector3(utm_x, utm_y, -gps_msg->altitude)); // gps altitude is negative (in a NED frame)
+  double relative_altitude = gps_msg->altitude - initial_gps_fix.altitude; // gps altitude in NED frame
+  utm_base_link_tf.setOrigin(tf2::Vector3(utm_x, utm_y, relative_altitude));
 
   // rotation of utm -> base_link transformation
   tf2::Quaternion tf2_quat;
@@ -137,7 +137,7 @@ void GPSTF::syncCB(const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
 
   geometry_msgs::TransformStamped datum_base_link_tfs;
   datum_base_link_tfs.header.stamp = time_stamp;
-  datum_base_link_tfs.header.frame_id = "datum";
+  datum_base_link_tfs.header.frame_id = world_frame;
   datum_base_link_tfs.child_frame_id = base_frame;
   datum_base_link_tfs.transform = tf2::toMsg(datum_base_link_tf);
   tf_broadcaster.sendTransform(datum_base_link_tfs);
@@ -145,7 +145,7 @@ void GPSTF::syncCB(const sensor_msgs::NavSatFix::ConstPtr& gps_msg,
   // also publish results as odom message
 
   geometry_msgs::PoseStamped pose;
-  pose.header.frame_id = "datum";
+  pose.header.frame_id = world_frame;
   pose.header.stamp = time_stamp;
   tf2::toMsg(datum_base_link_tf, pose.pose);
   // TODO covariance?
