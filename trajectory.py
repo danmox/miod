fc49a2cc3d347e0daadb9cc334067b489b77bbc9
Your Name <msi@example.com>
2020-04-06 17:29:12 -0300

    trajectory test added, on roslaunch_test_M.py - it uses simple_spawn_M.lunch and the functions implemented in trajectory.py

---
+++ trajectory.py
@@ -0,0 +1,52 @@
+import actionlib
+import copy
+from geometry_msgs.msg import Pose
+from intel_aero_navigation.msg import WaypointNavigationAction, WaypointNavigationGoal
+import rospy
+
+def fetch_param_warn(name):
+    try:
+        value = rospy.get_param(name)
+        return value
+    except KeyError:
+        rospy.logwarn('failed to fetch param \'%s\' from parameter server; using a default value of %s' % (name))
+
+
+
+
+def run_trajectory(id):
+    name = '/aero%d/gazebo_vel_nav_nodelet' % (id)
+#    name = '/aero%d/vel_nav_nodelet' % (id)
+    desired_trajectory = fetch_param_warn(name +'/desired_trajectory')
+    waypoint_count= len(desired_trajectory)
+    #
+    # send goals (from /aero%id/params) to id quad
+    #
+
+    try:
+        rospy.init_node('trajectory_node')
+        client = actionlib.SimpleActionClient(name, WaypointNavigationAction)
+        rospy.loginfo('waiting for %s to start' % (name))
+        client.wait_for_server()
+        goal = WaypointNavigationGoal()
+        goal.header.frame_id = 'world'
+        goal.header.stamp = rospy.get_rostime()
+        goal.end_action = WaypointNavigationGoal.LAND
+
+        for j in range(waypoint_count):
+            pose = Pose()
+            #pose =desired_trajectory[j]
+            pose.position.x =desired_trajectory[j][0]
+            pose.position.y =desired_trajectory[j][1]
+            pose.position.z =desired_trajectory[j][2]
+            pose.orientation.w = 1.0
+
+            goal.waypoints += [copy.deepcopy(pose)]
+
+            client.send_goal(goal)
+
+        rospy.loginfo('waiting for aero%s to complete' % (id))
+        client.wait_for_result()
+
+    except rospy.ROSInterruptException:
+        pass
