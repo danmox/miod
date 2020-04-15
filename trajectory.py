83a0a213fe6fc74e627cb001aa259f50de6651aa
Your Name <msi@example.com>
2020-04-15 16:41:54 -0300

    Mariana's added discussed changes to trajectory test case files

--- trajectory.py
+++ trajectory.py
@@ -14,39 +14,36 @@ def fetch_param_warn(name):
 
 
 
-def run_trajectory(id):
-    name = '/aero%d/gazebo_vel_nav_nodelet' % (id)
-#    name = '/aero%d/vel_nav_nodelet' % (id)
+def run_trajectory(id, dname, node):
+    name = '/'+ dname +str(id)+'/'+ node
     desired_trajectory = fetch_param_warn(name +'/desired_trajectory')
     waypoint_count= len(desired_trajectory)
     #
     # send goals (from /aero%id/params) to id quad
     #
 
-    try:
-        rospy.init_node('trajectory_node')
-        client = actionlib.SimpleActionClient(name, WaypointNavigationAction)
-        rospy.loginfo('waiting for %s to start' % (name))
-        client.wait_for_server()
-        goal = WaypointNavigationGoal()
-        goal.header.frame_id = 'world'
-        goal.header.stamp = rospy.get_rostime()
-        goal.end_action = WaypointNavigationGoal.LAND
+    rospy.init_node('trajectory_node')
+    client = actionlib.SimpleActionClient(name, WaypointNavigationAction)
+    rospy.loginfo('waiting for %s to start' % (name))
+    client.wait_for_server()
+    goal = WaypointNavigationGoal()
+    goal.header.frame_id = 'world'
+    goal.header.stamp = rospy.get_rostime()
+    goal.end_action = WaypointNavigationGoal.LAND
 
-        for j in range(waypoint_count):
-            pose = Pose()
-            #pose =desired_trajectory[j]
-            pose.position.x =desired_trajectory[j][0]
-            pose.position.y =desired_trajectory[j][1]
-            pose.position.z =desired_trajectory[j][2]
-            pose.orientation.w = 1.0
+    for j in range(waypoint_count):
+        pose = Pose()
+        #pose =desired_trajectory[j]
+        pose.position.x =desired_trajectory[j][0]
+        pose.position.y =desired_trajectory[j][1]
+        pose.position.z =desired_trajectory[j][2]
+        pose.orientation.w = 1.0
 
-            goal.waypoints += [copy.deepcopy(pose)]
+        goal.waypoints += [copy.deepcopy(pose)]
 
-            client.send_goal(goal)
+        client.send_goal(goal)
 
-        rospy.loginfo('waiting for aero%s to complete' % (id))
-        client.wait_for_result()
+    rospy.loginfo('waiting for aero%s to complete' % (id))
 
-    except rospy.ROSInterruptException:
-        pass
+#    client.wait_for_result()
+    return client
