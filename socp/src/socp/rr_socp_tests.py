e0b344999dfbd079fe532df01e859891473fabb5
Daniel Mox <mox@seas.upenn.edu>
2020-05-05 15:42:03 -0400

    consolidate route line generation

--- socp/src/socp/rr_socp_tests.py
+++ socp/src/socp/rr_socp_tests.py
@@ -76,51 +76,51 @@ def plot_config(config, ax=None, pause=None, clear_axes=False, show=True,
 
     if routes is not None:
 
-        # form dict of all route lines
+        # form dict of all route lines to be plotted later
         cumulative_routes = np.sum(routes, 2)
-        route_lines = []
-        for i in range(len(x)):
-            for j in range(i+1,len(x)):
-                Pi, Pj = np.asarray([x[i], y[i]]), np.asarray([x[j], y[j]])
-                Aij, Aji = cumulative_routes[i,j], cumulative_routes[j,i]
+        lines = []
+        for i, j in [(i,j) for i in range(len(x)) for j in range(i+1,len(x))]:
+            Pi = np.asarray([x[i], y[i]])
+            Pj = np.asarray([x[j], y[j]])
+            Aij = cumulative_routes[i,j]
+            Aji = cumulative_routes[j,i]
 
-                if Pj[0] < Pi[0] or Pj[1] < Pi[1]:
-                    Pi, Pj = Pj, Pi
-                    Aij, Aji = Aji, Aij
+            # ensures the arrows are oriented correctly
+            if Pj[0] < Pi[0] or Pj[1] < Pi[1]:
+                Pi, Pj = Pj, Pi
+                Aij, Aji = Aji, Aij
 
-                a1 = np.arctan2(Pj[1]-Pi[1], Pj[0]-Pi[0])
-                a2 = np.arctan2(Pi[1]-Pj[1], Pi[0]-Pj[0])
+            a1 = np.arctan2(Pj[1]-Pi[1], Pj[0]-Pi[0])
+            a2 = np.arctan2(Pi[1]-Pj[1], Pi[0]-Pj[0])
 
-                ds = np.pi / 16.0
-                offset_scale = 0.03 * window_scale
-                l1p1 = Pi + offset_scale*np.asarray([np.cos(a1+ds), np.sin(a1+ds)])
-                l2p1 = Pi + offset_scale*np.asarray([np.cos(a1-ds), np.sin(a1-ds)])
-                l1p2 = Pj + offset_scale*np.asarray([np.cos(a2-ds), np.sin(a2-ds)])
-                l2p2 = Pj + offset_scale*np.asarray([np.cos(a2+ds), np.sin(a2+ds)])
+            # line segment endpoints
+            ds = np.pi / 16.0
+            scale = 0.03 * window_scale
+            l1 = np.zeros((2,2))
+            l2 = np.zeros((2,2))
+            l1[0,:] = Pi + scale*np.asarray([np.cos(a1+ds), np.sin(a1+ds)])
+            l1[1,:] = Pj + scale*np.asarray([np.cos(a2-ds), np.sin(a2-ds)])
+            l2[0,:] = Pi + scale*np.asarray([np.cos(a1-ds), np.sin(a1-ds)])
+            l2[1,:] = Pj + scale*np.asarray([np.cos(a2+ds), np.sin(a2+ds)])
 
-                arrowhead_scale = 0.04 * window_scale
-                ds = np.pi / 8.0
-                l1head = l1p1 + arrowhead_scale*np.asarray([np.cos(a1+ds), np.sin(a1+ds)])
-                l2head = l2p2 + arrowhead_scale*np.asarray([np.cos(a2+ds), np.sin(a2+ds)])
+            # arrowhead endpoint
+            ds = np.pi / 8.0
+            scale = 0.04 * window_scale
+            h1 = l1[0,:] + scale*np.asarray([np.cos(a1+ds), np.sin(a1+ds)])
+            h2 = l2[1,:] + scale*np.asarray([np.cos(a2+ds), np.sin(a2+ds)])
 
-                if Aji > 0.01:
-                    route_lines.append({'rate': Aji,
-                                        'line_x': [l1p1[0], l1p2[0]],
-                                        'line_y': [l1p1[1], l1p2[1]],
-                                        'arrow_x': [l1p1[0], l1head[0]],
-                                        'arrow_y': [l1p1[1], l1head[1]]})
-                if Aij > 0.01:
-                    route_lines.append({'rate': Aij,
-                                        'line_x': [l2p1[0], l2p2[0]],
-                                        'line_y': [l2p1[1], l2p2[1]],
-                                        'arrow_x': [l2p2[0], l2head[0]],
-                                        'arrow_y': [l2p2[1], l2head[1]]})
+            if Aji > 0.01:
+                lines.append({'rate': Aji, 'line_x': l1[:,0], 'line_y': l1[:,1],
+                              'arrow_x': [l1[0,0], h1[0]], 'arrow_y': [l1[0,1], h1[1]]})
+            if Aij > 0.01:
+                lines.append({'rate': Aij, 'line_x': l2[:,0], 'line_y': l2[:,1],
+                              'arrow_x': [l2[1,0], h2[0]], 'arrow_y': [l2[1,1], h2[1]]})
 
         # plot lines by line weight: faintest on the bottom, boldest on top
         lw = 2
         cmap = cm.ScalarMappable(norm=Normalize(vmin=0.0, vmax=1.0, clip=True), cmap='YlOrBr')
-        route_lines.sort(key=lambda line_dict: line_dict['rate'])
-        for d in route_lines:
+        lines.sort(key=lambda line_dict: line_dict['rate'])
+        for d in lines:
             ax.plot(d['line_x'], d['line_y'], lw=lw, c=cmap.to_rgba(d['rate']))
             ax.plot(d['arrow_x'], d['arrow_y'], lw=lw, c=cmap.to_rgba(d['rate']))
 
